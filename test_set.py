6bd4918d34b5af43a295e768607eee1ee4b81bda
RG Aquino <ralphgregoraquino@gmail.com>
2018-03-11 16:57:30 +0800

    Initial commit.

---
+++ test_set.py
@@ -0,0 +1,20 @@
+import numpy as np
+import hashlib
+
+
+def split_train_test(data, test_ratio):
+    shuffle_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffle_indices[:test_set_size]
+    train_indices = shuffle_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
